9d55066dafba0c3bd3da7a78390529b2324dfb5d
Wing Chau <wingchau@master-dynamic.com>
2019-10-16 07:07:20 +0000

    Add IBM Configuration Package
    - Added IMB Configuration as dependency to read config from path.

--- Package.swift
+++ Package.swift
@@ -8,13 +8,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/IBM-Swift/Configuration.git",
+            from: "3.0.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MarketDataFetchers",
-            dependencies: []),
+            dependencies: ["Configuration"]),
         .testTarget(
             name: "MarketDataFetchersTests",
             dependencies: ["MarketDataFetchers"]),
