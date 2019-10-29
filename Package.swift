7597a893d368c04a45c37a4036a84ed1458fd261
Wing Chau <wingchau@master-dynamic.com>
2019-10-29 07:35:08 +0000

    Install SwiftyRequest
    - Added `SwiftyRequest` v3.0.0 as dependence in `Package.swift` for
    restful API calls.

--- Package.swift
+++ Package.swift
@@ -10,16 +10,21 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/IBM-Swift/Configuration.git",
-            from: "3.0.4")
+            from: "3.0.4"
+        ),
+        .package(url: "https://github.com/IBM-Swift/SwiftyRequest",
+                 from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AlphaVantage",
-            dependencies: ["Configuration"]),
+            dependencies: ["Configuration", "SwiftyRequest"]
+        ),
         .testTarget(
             name: "AlphaVantageTests",
-            dependencies: ["AlphaVantage"]),
+            dependencies: ["AlphaVantage"]
+        ),
     ]
 )
